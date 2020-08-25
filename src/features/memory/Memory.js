c7188dddca86ca06f4a6328f5dfffb69da2b0ccf
Jason Fellows <jasonfellows@gmail.com>
2020-08-25 18:49:33 +0200

    Remove example feature. Init Memory feature.

---
+++ src/features/memory/Memory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Memory.module.css'
+
+export function Memory () {
+  return (
+    <>
+      <header className={styles.header} />
+      <main className={styles.main} />
+    </>
+  )
+}
